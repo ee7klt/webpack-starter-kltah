351fe919b3c4b3b628181164a042e60f2cd911e9
ee7klt <kltah1@gmail.com>
2015-11-17 09:50:04 +0800

    initial commit

---
+++ src/app/components/main.jsx
@@ -0,0 +1,15 @@
+/** In this file, we create a React component which incorporates components provided by material-ui */
+
+const React = require('react');
+
+
+const Main = React.createClass({
+
+  render () {
+    return <div>
+      <h1>This is KL Tah's Webpack Starter!</h1>
+      </div>
+  },
+});
+
+module.exports = Main;
